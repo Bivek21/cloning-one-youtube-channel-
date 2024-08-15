6d5dec373e8ed59a6784f5d9e72a66beff15c94f
Bivek Baral <baralbibek1234@gmail.com>
2024-08-15 22:46:43 +0930

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+createCard(
+  "Introduction to Backend | Sigma Web Dev Video #2",
+  "CodeWithHarry",
+  560000,
+  7,
+  "31.22",
+  "https://i.ytimg.com/vi/UzYRQURh_pY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD8HTf9ccCrBVaK84uFwvZuwrLcng"
+);
